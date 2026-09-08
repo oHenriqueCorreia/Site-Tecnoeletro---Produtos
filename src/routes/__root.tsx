c7657695b022dfa56f7b73df43e840a063641bb5
Henrique Correia <henrique@tecnoeletro.com>
2026-09-08 16:14:58 -0300

    atualização bug

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -116,34 +116,24 @@ function RootShell({ children }: { children: ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <!-- Google Tag Manager -->
-        <script>
-          (function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != "dataLayer" ? "&l=" + l : "";
-            j.async = true;
-            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, "script", "dataLayer", "GTM-KHV89NQ5");
-        </script>
-        <!-- End Google Tag Manager -->
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KHV89NQ5');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
 
-        <!-- Google tag (gtag.js) -->
+        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17995376071"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag() {
-            dataLayer.push(arguments);
-          }
-          gtag("js", new Date());
-          gtag("config", "AW-17995376071");
-        </script>
-        <!-- End Google tag (gtag.js) -->
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-17995376071');`,
+          }}
+        />
+        {/* End Google tag (gtag.js) */}
 
-        <!-- Google Tag Manager (noscript) -->
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KHV89NQ5"
@@ -152,7 +142,7 @@ function RootShell({ children }: { children: ReactNode }) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-        <!-- End Google Tag Manager (noscript) -->
+        {/* End Google Tag Manager (noscript) */}
 
         {children}
         <Scripts />
